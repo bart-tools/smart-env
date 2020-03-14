3db75f0885f537a199073c7aae8420eaddbcbe02
Alex Sokolov <volokos.alex@gmail.com>
2020-03-14 19:09:59 +0200

    SE-13: Added operator "in"

--- tests/test_decoders.py
+++ tests/test_decoders.py
@@ -24,8 +24,12 @@ THE SOFTWARE.
 
 import unittest
 
-
+from smart_env.decoders import BooleanDecoder
+from smart_env.decoders import CollectionDecoder
 from smart_env.decoders import IDecoder
+from smart_env.decoders import JSONDecoder
+from smart_env.decoders import SUPPORTED_DECODERS
+from smart_env.exceptions import EncodeError
 
 
 __all__ = ('DecoderTestCase',)
@@ -47,3 +51,78 @@ class DecoderTestCase(unittest.TestCase):
         """Check that IDecoder is a true interface"""
         with self.assertRaises(NotImplementedError):
             FakeDecoder.decode(None)
+
+
+class EncoderTestCase(unittest.TestCase):
+    """Test cases for encoders"""
+
+    def setUp(self):
+        self.initial_data = [
+            {
+                'decoder': JSONDecoder,
+                'values': [
+                    {
+                        'input': {"abc": True, "def": 10},
+                        'output': [
+                            '{"abc": true, "def": 10}',
+                            '{"def": 10, "abc": true}'
+                        ]
+                    },
+                    {
+                        'input': [1, 2, 3, "asd", False],
+                        'output': '[1, 2, 3, "asd", false]'
+                    },
+                    {
+                        'input': [1, 2, 34, None],
+                        'output': '[1, 2, 34, null]'
+                    }
+                ],
+            },
+            {
+                'decoder': BooleanDecoder,
+                'values': [
+                    {
+                        'input': True,
+                        'output': 'true'
+                    },
+                    {
+                        'input': False,
+                        'output': 'false'
+                    }
+                ]
+            },
+            {
+                'decoder': CollectionDecoder,
+                'values': [
+                    {
+                        'input': {1, 3},
+                        'output': [
+                            '[1, 3]',
+                            '[3, 1]'
+                        ]
+                    },
+                    {
+                        'input': frozenset({1, 2}),
+                        'output': [
+                            '[1, 2]',
+                            '[2, 1]'
+                        ]
+                    },
+                    {
+                        'input': (1, 2, 3.14),
+                        'output': '[1, 2, 3.14]'
+                    }
+                ]
+            }
+        ]
+
+    def test_encoding_mechanism(self):
+        """Check if all supported decoders can encode data"""
+
+        for context in self.initial_data:
+            for input_data in context['values']:
+                output = context['decoder'].encode(input_data['input'])
+                if isinstance(input_data['output'], list):
+                    self.assertIn(output, input_data['output'])
+                else:
+                    self.assertEqual(output, input_data['output'])
